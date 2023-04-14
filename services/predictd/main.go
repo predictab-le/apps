8c755e3153456a30e9ceccbdf5cf001230bcc24b
George MacRorie <me@georgemac.com>
2023-04-14 14:35:59 +0100

    Initial dummy predictd service

--- services/predictd/main.go
+++ services/predictd/main.go
@@ -4,41 +4,13 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-
-	"go.flipt.io/flipt/rpc/flipt"
-	sdk "go.flipt.io/flipt/sdk/go"
 )
 
 func main() {
-	var (
-		features = sdk.New(nil).Flipt()
-		db       *sql.DB
-		openAI   *http.Client
-	)
+	var db *sql.DB
 
 	http.HandleFunc("/predict", func(w http.ResponseWriter, r *http.Request) {
-		resp, err := features.Evaluate(r.Context(), &flipt.EvaluationRequest{
-			FlagKey: "predictionSource",
-			Context: map[string]string{
-				"organization": getOrganization(r.Context()),
-			},
-		})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		var prediction Prediction
-		switch resp.Value {
-		case "fromDB":
-			prediction, err = predictionFromDB(db)
-		case "fromOpenAI":
-			prediction, err = predictionFromOpenAI(openAI)
-		default:
-			http.Error(w, "something went wrong", http.StatusInternalServerError)
-			return
-		}
-
+		prediction, err := predictionFromDB(db)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
